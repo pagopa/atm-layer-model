41ccd9669ae809a56c1f9f91b8e537f4ce7e7342
Elis <elis.kina@nttdata.com>
2023-11-08 09:36:18 +0100

    More JUNIT Tests

---
+++ src/test/java/it/gov/pagopa/atmlayer/service/model/model/ATMLayerErrorResponseTest.java
@@ -0,0 +1,69 @@
+package it.gov.pagopa.atmlayer.service.model.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@QuarkusTest
+public class ATMLayerErrorResponseTest {
+
+  @Inject
+  ObjectMapper objectMapper;
+
+  @Test
+  public void testATMLayerErrorResponseSerialization() throws IOException {
+    ATMLayerErrorResponse errorResponse = ATMLayerErrorResponse.builder()
+        .errorCode("E001")
+        .type("Validation Error")
+        .statusCode(500)
+        .message("Invalid input data")
+        .build();
+
+    String json = objectMapper.writeValueAsString(errorResponse);
+    ATMLayerErrorResponse deserializedErrorResponse = objectMapper.readValue(json, ATMLayerErrorResponse.class);
+    assertNotEquals(errorResponse, deserializedErrorResponse);
+  }
+
+  @Test
+  public void testJsonPropertyOrder() {
+    ATMLayerErrorResponse errorResponse = ATMLayerErrorResponse.builder()
+        .errorCode("E001")
+        .type("Validation Error")
+        .statusCode(500)
+        .message("Invalid input data")
+        .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      String json = objectMapper.writeValueAsString(errorResponse);
+      assertTrue(json.contains("\"type\":\"Validation Error\""));
+      assertTrue(json.contains("\"errorCode\":\"E001\""));
+      assertTrue(json.contains("\"statusCode\":500"));
+      assertTrue(json.contains("\"message\":\"Invalid input data\""));
+    } catch (Exception e) {
+      fail("JSON serialization failed: " + e.getMessage());
+    }
+  }
+
+  @Test
+  public void testGetterAnnotations() {
+    ATMLayerErrorResponse errorResponse = ATMLayerErrorResponse.builder()
+        .errorCode("E001")
+        .type("Validation Error")
+        .statusCode(500)
+        .message("Invalid input data")
+        .build();
+
+    assertEquals("E001", errorResponse.getErrorCode());
+    assertEquals("Validation Error", errorResponse.getType());
+    assertEquals(500, errorResponse.getStatusCode());
+    assertEquals("Invalid input data", errorResponse.getMessage());
+  }
+}
---
+++ src/test/java/it/gov/pagopa/atmlayer/service/model/model/ErrorResponseTest.java
@@ -0,0 +1,52 @@
+package it.gov.pagopa.atmlayer.service.model.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@QuarkusTest
+public class ErrorResponseTest {
+
+  @Inject
+  ObjectMapper objectMapper;
+
+  @Test
+  public void testErrorResponseSerialization() throws Exception {
+    ErrorResponse errorResponse = ErrorResponse.builder()
+        .type("error")
+        .title("Internal Server Error")
+        .status(500)
+        .detail("An unexpected error has occurred. Please contact support.")
+        .instance("ATMLM-500")
+        .build();
+
+    String json = objectMapper.writeValueAsString(errorResponse);
+    ErrorResponse deserializedErrorResponse = objectMapper.readValue(json, ErrorResponse.class);
+    assertNotEquals(errorResponse, deserializedErrorResponse);
+  }
+
+  @Test
+  public void testJsonPropertyOrder() throws Exception {
+    ErrorResponse errorResponse = ErrorResponse.builder()
+        .type("error")
+        .title("Internal Server Error")
+        .status(500)
+        .detail("An unexpected error has occurred. Please contact support.")
+        .instance("ATMLM-500")
+        .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      String json = objectMapper.writeValueAsString(errorResponse);
+      String expectedJson = "{\"type\":\"error\",\"title\":\"Internal Server Error\",\"status\":500,\"detail\":\"An unexpected error has occurred. Please contact support.\",\"instance\":\"ATMLM-500\"}";
+      assertEquals(expectedJson, json);
+    } catch (Exception e) {
+      fail("JSON serialization failed: " + e.getMessage());
+    }
+  }
+}
