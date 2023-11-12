b498ed156bb267ba0c06f93318f922f9a38ba335
Cocuzzoli, Andrea <andrea.cocuzzoli@nttdata.com>
2023-11-12 03:31:12 +0100

    added integration tests execution in maven

---
+++ src/test/java/it/gov/pagopa/atmlayer/service/model/integrationtests/IntegrationTests.java
@@ -0,0 +1,42 @@
+package it.gov.pagopa.atmlayer.service.model.integrationtests;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
+
+@QuarkusTest
+@QuarkusTestResource(value = EnviromentTestServicesResource.DockerCompose.class, restrictToAnnotatedClass = true)
+@Slf4j
+public class IntegrationTests {
+
+    private static final GenericContainer<?> NEWMAN = new GenericContainer<>(new ImageFromDockerfile()
+            .withDockerfile(Paths.get("src/test/resources/integration-test/Dockerfile-postman")))
+            .withFileSystemBind("src/test/resources/integration-test/output", "/output", BindMode.READ_WRITE)
+            .withAccessToHost(true)
+            .withStartupCheckStrategy(new OneShotStartupCheckStrategy().withTimeout(Duration.ofSeconds(120)));
+
+    @BeforeAll
+    static void exposeTestPort() {
+        Testcontainers.exposeHostPorts(8086);
+    }
+
+    @Test
+    void executePostmanCollectionWithNewmann() {
+        NEWMAN.start();
+        log.info(NEWMAN.getLogs());
+        assertTrue(NEWMAN.getCurrentContainerInfo().getState().getExitCodeLong() == 0);
+    }
+
+}
