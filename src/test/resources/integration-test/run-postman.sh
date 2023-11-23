f49de4d09217dedadd993e8d5dc095959d879db6
Elis <elis.kina@nttdata.com>
2023-11-23 17:01:22 +0100

    logback-test.xml , result.html, result.json

--- src/test/resources/integration-test/run-postman.sh
+++ src/test/resources/integration-test/run-postman.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 newman run /postman/Integration_test_model.postman_collection.json -r cli,json,htmlextra \
 --reporter-json-export output/result.json --reporter-htmlextra-export output/result.html \
 --env-var "baseUrl=http://host.testcontainers.internal:8086"
