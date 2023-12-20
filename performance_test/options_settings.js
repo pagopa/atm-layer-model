173c00a1ceeee72a4d753aa87bced620ecf64ff8
Gabriele Maiocchi <107944380+GabrieleMaiocchiFilippo@users.noreply.github.com>
2023-12-20 17:59:54 +0100

    added log and updated treshold (#104)

--- performance_test/options_settings.js
+++ performance_test/options_settings.js
@@ -4,8 +4,8 @@ export const thresholdsSettings = {
 };
 
 export const nameThresholds={
-  'http_req_duration{name:BPMNcreate}': ['p(95)<500'],
-  'http_req_waiting{name:BPMNcreate}':['p(95)<1000'],
+  'http_req_duration{name:BPMNcreate}': ['p(95)<10000'],
+  'http_req_waiting{name:BPMNcreate}':['p(95)<10000'],
   'http_req_failed{name:BPMNcreate}':['rate<0.01'],
   'http_reqs{name:BPMNcreate}':[],
 
@@ -14,30 +14,30 @@ export const nameThresholds={
   // 'http_req_failed{name:BPMNgetAll}':['rate<0.01'],
   // 'http_reqs{name:BPMNgetAll}':[],
 
-  'http_req_duration{name:BPMNdeploy}': ['p(95)<1500'],
-  'http_req_waiting{name:BPMNdeploy}':['p(95)<2000'],
+  'http_req_duration{name:BPMNdeploy}': ['p(95)<10000'],
+  'http_req_waiting{name:BPMNdeploy}':['p(95)<10000'],
   // 'http_req_failed{name:BPMNdeploy}':['rate<0.01'],
   'http_reqs{name:BPMNdeploy}':[],
 
-  'http_req_duration{name:BPMNassociate}': ['p(95)<1500'],
-  'http_req_waiting{name:BPMNassociate}':['p(95)<1000'],
+  'http_req_duration{name:BPMNassociate}': ['p(95)<10000'],
+  'http_req_waiting{name:BPMNassociate}':['p(95)<10000'],
   'http_req_failed{name:BPMNassociate}':['rate<0.01'],
   'http_reqs{name:BPMNassociate}':[],
 
-  'http_req_duration{name:BPMNupgrade}': ['p(95)<1500'],
-  'http_req_waiting{name:BPMNupgrade}':['p(95)<1000'],
+  'http_req_duration{name:BPMNupgrade}': ['p(95)<10000'],
+  'http_req_waiting{name:BPMNupgrade}':['p(95)<10000'],
   'http_req_failed{name:BPMNupgrade}':['rate<0.01'],
   'http_reqs{name:BPMNupgrade}':[],
 
   // RESOURCES
 
-  'http_req_duration{name:RESOURCECreate}': ['p(95)<1500'],
-  'http_req_waiting{name:RESOURCECreate}':['p(95)<1000'],
+  'http_req_duration{name:RESOURCECreate}': ['p(95)<10000'],
+  'http_req_waiting{name:RESOURCECreate}':['p(95)<10000'],
   'http_req_failed{name:RESOURCECreate}':['rate<0.01'],
   'http_reqs{name:RESOURCECreate}':[],
 
-  'http_req_duration{name:RESOURCEUpdate}': ['p(95)<1500'],
-  'http_req_waiting{name:RESOURCEUpdate}':['p(95)<1000'],
+  'http_req_duration{name:RESOURCEUpdate}': ['p(95)<10000'],
+  'http_req_waiting{name:RESOURCEUpdate}':['p(95)<10000'],
   'http_req_failed{name:RESOURCEUpdate}':['rate<0.01'],
   'http_reqs{name:RESOURCEUpdate}':[]
 }
