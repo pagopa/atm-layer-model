1dd1f87bc6a4afcaf08cb508dc27a59e703e83a8
LuciaM1 <144435187+LuciaM1@users.noreply.github.com>
2024-01-26 13:26:56 +0100

    added associations to get filters (#117)

--- src/main/java/it/gov/pagopa/atmlayer/service/model/repository/BpmnVersionRepository.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/model/repository/BpmnVersionRepository.java
@@ -17,10 +17,12 @@ public class BpmnVersionRepository implements PanacheRepositoryBase<BpmnVersion,
         String queryFilters = params.keySet().stream().map(key -> {
                 if (Objects.equals(key, "modelVersion") || Objects.equals(key, "definitionVersionCamunda")) {
                     return ("b." + key + " = :"+key);
-        } else {
+        } else if (Objects.equals(key, "acquirerId")||Objects.equals(key, "branchId")||Objects.equals(key, "terminalId")){
+                    return ("bc.bpmnBankConfigPK."+key + " = :"+key);
+                }else {
                     return ("b." + key + " LIKE concat(concat('%', :"+key+"), '%')");}
         }).collect(Collectors.joining(" and "));
-        return find(("select b from BpmnVersion b ").concat(queryFilters.isBlank()?"":" where " + queryFilters), params).page(Page.of(pageIndex,pageSize)).list();
+        return find(("select b from BpmnVersion b join BpmnBankConfig bc on b.bpmnId = bc.bpmnBankConfigPK.bpmnId and b.modelVersion = bc.bpmnBankConfigPK.bpmnModelVersion ").concat(queryFilters.isBlank()?"":" where " + queryFilters),params).page(Page.of(pageIndex,pageSize)).list();
     }
 
     public Uni<BpmnVersion> findBySHA256(String sha256) {
