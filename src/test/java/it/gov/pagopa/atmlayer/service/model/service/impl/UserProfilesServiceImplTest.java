4daeac9f26e310282d63724ffd9afe5478f966be
ElisKina-dev <elis.kina@nttdata.com>
2024-06-25 14:29:07 +0200

    added tests of UserProfilesServiceImpl

--- src/test/java/it/gov/pagopa/atmlayer/service/model/service/impl/UserProfilesServiceImplTest.java
+++ src/test/java/it/gov/pagopa/atmlayer/service/model/service/impl/UserProfilesServiceImplTest.java
@@ -3,14 +3,18 @@ package it.gov.pagopa.atmlayer.service.model.service.impl;
 import io.quarkus.test.junit.QuarkusTest;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
+import it.gov.pagopa.atmlayer.service.model.dto.UserProfilesInsertionDTO;
 import it.gov.pagopa.atmlayer.service.model.entity.Profile;
 import it.gov.pagopa.atmlayer.service.model.entity.User;
 import it.gov.pagopa.atmlayer.service.model.entity.UserProfiles;
 import it.gov.pagopa.atmlayer.service.model.entity.UserProfilesPK;
+import it.gov.pagopa.atmlayer.service.model.enumeration.AppErrorCodeEnum;
 import it.gov.pagopa.atmlayer.service.model.exception.AtmLayerException;
+import it.gov.pagopa.atmlayer.service.model.mapper.UserProfilesMapper;
 import it.gov.pagopa.atmlayer.service.model.repository.ProfileRepository;
 import it.gov.pagopa.atmlayer.service.model.repository.UserProfilesRepository;
 import it.gov.pagopa.atmlayer.service.model.repository.UserRepository;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -23,6 +27,7 @@ import java.util.concurrent.CompletionStage;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -32,6 +37,8 @@ class UserProfilesServiceImplTest {
     UserProfilesRepository userProfilesRepository;
     @Mock
     ProfileRepository profileRepository;
+    @Mock
+    UserProfilesMapper userProfilesMapper;
     @Mock
     UserRepository userRepository;
     @InjectMocks
@@ -180,4 +187,101 @@ class UserProfilesServiceImplTest {
         verify(userRepository, times(0)).findById(anyString());
     }
 
+    @Test
+    void testIsUserProfileExisting_existingProfile() {
+        UserProfilesPK userProfilesPK = new UserProfilesPK("userId", 1);
+        UserProfiles existingUserProfile = new UserProfiles(userProfilesPK);
+        when(userProfilesRepository.findById(userProfilesPK)).thenReturn(Uni.createFrom().item(existingUserProfile));
+
+        Uni<Boolean> result = userProfilesService.isUserProfileExisting(existingUserProfile);
+
+        assertTrue(result.await().indefinitely());
+
+        verify(userProfilesRepository, times(1)).findById(userProfilesPK);
+    }
+
+    @Test
+    void testIsUserProfileExisting_nonExistingProfile() {
+        UserProfilesPK userProfilesPK = new UserProfilesPK("userId", 1);
+        when(userProfilesRepository.findById(userProfilesPK)).thenReturn(Uni.createFrom().nullItem());
+
+        Uni<Boolean> result = userProfilesService.isUserProfileExisting(new UserProfiles(userProfilesPK));
+
+        assertFalse(result.await().indefinitely());
+
+        verify(userProfilesRepository, times(1)).findById(userProfilesPK);
+    }
+
+    @Test
+    void testDeleteUserProfiles_profileFound() {
+        UserProfilesPK userProfilesIDs = new UserProfilesPK("userId", 1);
+        UserProfiles existingUserProfile = new UserProfiles();
+        when(userProfilesRepository.findById(any(UserProfilesPK.class))).thenReturn(Uni.createFrom().item(existingUserProfile));
+        when(userProfilesRepository.delete(existingUserProfile)).thenReturn(Uni.createFrom().nullItem());
+
+        Uni<Void> result = userProfilesService.deleteUserProfiles(userProfilesIDs);
+
+        result.await().indefinitely();
+
+        verify(userProfilesRepository, times(1)).findById(userProfilesIDs);
+        verify(userProfilesRepository, times(1)).delete(existingUserProfile);
+    }
+
+    @Test
+    void testDeleteUserProfiles_profileNotFound() {
+        UserProfilesPK userProfilesIDs = new UserProfilesPK("userId", 1);
+        when(userProfilesRepository.findById(any(UserProfilesPK.class))).thenReturn(Uni.createFrom().nullItem());
+
+        Uni<Void> result = userProfilesService.deleteUserProfiles(userProfilesIDs);
+
+        AtmLayerException exception = assertThrows(AtmLayerException.class, () -> result.await().indefinitely());
+
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), exception.getStatusCode());
+        assertEquals(AppErrorCodeEnum.NO_ASSOCIATION_FOUND.getErrorMessage(), exception.getMessage());
+
+        verify(userProfilesRepository, times(1)).findById(userProfilesIDs);
+    }
+
+
+    @Test
+    void testUpdateUserProfiles_UserNotFound() {
+        String userId = "1";
+        UserProfilesInsertionDTO userProfilesInsertionDTO = new UserProfilesInsertionDTO();
+        userProfilesInsertionDTO.setUserId(userId);
+        userProfilesInsertionDTO.setProfileIds(Arrays.asList(1, 2, 3));
+
+        when(userProfilesService.checkUser(userId)).thenReturn(Uni.createFrom().failure(new RuntimeException("User not found")));
+
+        Uni<List<UserProfiles>> resultUni = userProfilesService.updateUserProfiles(userProfilesInsertionDTO);
+
+        assertThrows(Exception.class, () -> resultUni.await().indefinitely());
+        verify(userProfilesRepository, never()).findByUserId(anyString());
+    }
+
+    /*@Test
+    void testUpdateUserProfiles_Success() {
+        String userId = "1";
+        UserProfilesInsertionDTO userProfilesInsertionDTO = new UserProfilesInsertionDTO();
+        userProfilesInsertionDTO.setUserId(userId);
+        userProfilesInsertionDTO.setProfileIds(Arrays.asList(1, 2, 3));
+
+        List<UserProfiles> userProfilesToUpdate = Arrays.asList(new UserProfiles(), new UserProfiles());
+
+        when(userProfilesRepository.findByUserId(userId)).thenReturn(Uni.createFrom().item(List.of(new UserProfiles())));
+        when(userProfilesMapper.toEntityInsertion(userProfilesInsertionDTO)).thenReturn(userProfilesToUpdate);
+        when(userProfilesRepository.deleteUserProfiles(any())).thenReturn(Uni.createFrom().item(1L));
+        doAnswer(invocation -> Uni.createFrom().nullItem()).when(userProfilesRepository).persist((UserProfiles) any());
+
+        Uni<List<UserProfiles>> resultUni = userProfilesService.updateUserProfiles(userProfilesInsertionDTO);
+
+        verify(userProfilesRepository).findByUserId(userProfilesInsertionDTO.getUserId());
+        verify(userProfilesRepository).deleteUserProfiles(any());
+        verify(userProfilesRepository).persist((UserProfiles) any());
+
+        resultUni
+                .subscribe()
+                .withSubscriber(UniAssertSubscriber.create())
+                .assertCompleted()
+                .assertItem(List.of(new UserProfiles()));
+    }*/
 }
