f1a9abf554cfff9a682b5e3736126161d73d6a73
EmVal <emanuele.valentini@nttdata.com>
2024-07-31 10:26:45 +0200

    Remove user and profiles

--- src/main/java/it/gov/pagopa/atmlayer/service/model/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package it.gov.pagopa.atmlayer.service.model.repository;
-
-import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
-import it.gov.pagopa.atmlayer.service.model.entity.Profile;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class ProfileRepository implements PanacheRepositoryBase<Profile, Integer> {
-}
